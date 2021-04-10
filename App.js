efb961d3d34cf071fde4e3baae6dfd78ca01853e
collinsbett29 <collinsbegtt29@gmail.com>
2021-04-10 10:02:08 +0300

    Add a applpoading component

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
@@ -18,10 +18,7 @@ const fetchFonts = () => {
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
-}; 
-
-
-
+};
 
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
@@ -33,7 +30,6 @@ export default function App() {
       onFinish={() => setFontLoaded(true)}
       onError={(err) => console.log(err)}
       /> 
-      
     );
   }
   return (
@@ -42,6 +38,5 @@ export default function App() {
     </Provider>
   );
 }
-
 
 
